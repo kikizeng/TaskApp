98131c006f915684bb9bdefbd47b1e5ed65a5a31
Kikizeng <kiki@josephmark.com.au>
2023-02-21 19:41:08 +1000

    landing page and design system set up

--- App.tsx
+++ App.tsx
@@ -1,20 +1,148 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import {
+  Button,
+  DefaultTheme,
+  MD3LightTheme,
+  Provider as PaperProvider,
+} from "react-native-paper";
+import * as Font from "expo-font";
+import { useState } from "react";
+import AppLoading from "expo-app-loading";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const loadFonts = async () => {
+  await Font.loadAsync({
+    "syne-semibold": require("./assets/fonts/Syne-SemiBold.ttf"),
+    "syne-regular": require("./assets/fonts/Syne-Regular.ttf"),
+  });
+};
+
+type Theme = typeof DefaultTheme;
+// Design system
+const theme = {
+  ...DefaultTheme,
+  fonts: {
+    ...DefaultTheme.fonts,
+    regular: {
+      fontFamily: "syne-regular",
+    },
+    semibold: {
+      fontFamily: "syne-semibold",
+      fontWeight: "semibold",
+    },
+  },
+  colors: {
+    ...DefaultTheme.colors,
+    white: "#F8F8F8",
+    charcoal: "#383939",
+    offWhite: "#FFFFFF",
+    lightGrey: "#E8E8E8",
+    black: "#000000",
+    darkGrey: "#707070",
+    blue: "#6537FF",
+    error: "#B00020",
+  },
+};
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  if (!fontsLoaded) {
+    return (
+      <AppLoading
+        startAsync={loadFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View>
+        <View style={{ width: 414, height: undefined }}>
+          <Image
+            source={require("./assets/mask-group.png")}
+            style={{ width: "100%", height: undefined, aspectRatio: 1 }}
+          />
+        </View>
+        <View
+          style={{
+            paddingTop: 51,
+            paddingLeft: 30,
+            paddingRight: 30,
+            alignItems: "flex-start",
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: "syne-semibold",
+              color: theme.colors.charcoal,
+              fontSize: 25,
+            }}
+          >
+            Welcome to
+          </Text>
+          <Text
+            style={{
+              fontFamily: "syne-semibold",
+              color: theme.colors.charcoal,
+              fontSize: 25,
+            }}
+          >
+            the NBM Forum
+          </Text>
+          <Text
+            style={{
+              fontFamily: "syne-regular",
+              color: theme.colors.charcoal,
+              fontSize: 14,
+            }}
+          >
+            Time to get all the answers you need in a forum made for designers
+            and developers!
+          </Text>
+        </View>
+        <View
+          style={{
+            paddingTop: 39,
+            paddingLeft: 30,
+            paddingRight: 30,
+            alignItems: "flex-start",
+          }}
+        >
+          <Button
+            style={{
+              width: 320,
+              backgroundColor: theme.colors.blue,
+              alignItems: "center",
+              borderRadius: 0,
+              borderColor: theme.colors.charcoal,
+            }}
+            mode="contained"
+            onPress={() => console.log("clicked")}
+          >
+            <Text
+              style={{
+                fontFamily: "syne-regular",
+                fontSize: 14,
+              }}
+            >
+              Jump In!
+            </Text>
+          </Button>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F8F8F8",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
